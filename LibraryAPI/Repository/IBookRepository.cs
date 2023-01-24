b5f055bf1769ee1f4be64d11f8bbfdd0cdb46da1
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-01-24 22:53:29 +0300

    Book and Member Repositories Added

---
+++ LibraryAPI/Repository/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using LibraryAPI.Models;
+using System.Linq.Expressions;
+using System;
+
+namespace LibraryAPI.Repository;
+
+public interface IBookRepository
+{
+    Task<IEnumerable<Book>> GetAllBooksAsync();
+    Task<IEnumerable<Book>> GetManyAsync(Expression<Func<Book, bool>> predicate);
+    Task<Book> GetBookByIdAsync(Guid id);
+    Task AddBookAsync(Book book);
+    Task UpdateBookAsync(Book book);
+    Task DeleteBookAsync(Book book);
+    Task<bool> SaveChangesAsync();
+    
+}
---
+++ LibraryAPI/Repository/IMemberRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryAPI.Models;
+using System.Linq.Expressions;
+using System;
+
+namespace LibraryAPI.Repository;
+
+public interface IMemberRepository
+{
+    Task<IEnumerable<Member>> GetAllUsersAsync();
+    Task<Member> GetUserByIdAsync(int id);
+    Task AddUserAsync(Member user);
+    Task UpdateUserAsync(Member user);
+    Task DeleteUserAsync(Member user);
+    Task<bool> SaveChangesAsync();
+
+}
