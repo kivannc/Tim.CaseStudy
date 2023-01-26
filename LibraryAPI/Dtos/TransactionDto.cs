868fb5d96f777d13e56998d4b2a398a97c6ed8a8
kivanc <erturkkivanc@gmail.com>
2023-01-26 11:33:05 +0300

    TransactionDto LateDays calculation fixed

--- LibraryAPI/Dtos/TransactionDto.cs
+++ LibraryAPI/Dtos/TransactionDto.cs
@@ -1,23 +1,22 @@
 ﻿using LibraryAPI.Models;
 
-namespace LibraryAPI.Dtos
+namespace LibraryAPI.Dtos;
+
+public class TransactionDto
 {
-    public class TransactionDto
-    {
-        public BookDto Book { get; set; }
+    public int Id { get; set; }
+    
+    public BookDto Book { get; set; }
 
-        public MemberDto Member { get; set; }
+    public MemberDto Member { get; set; }
 
-        public DateTime DueDate { get; set; }
+    public DateTime DueDate { get; set; }
 
-        public DateTime BorrowDate { get; set; }
+    public DateTime BorrowDate { get; set; }
 
-        public int LateDays => (int)(DateTime.Now - DueDate).TotalDays;
+    public int LateDays =>DateTime.Now > DueDate ?  (int)(DateTime.Now - DueDate).TotalDays : 0;
 
-        public double Penalty { get; set; }
+    public double Penalty { get; set; }
 
-        public BookStatus BookStatus { get; set; }
-
-
-    }
+    public BookStatus BookStatus { get; set; }
 }
