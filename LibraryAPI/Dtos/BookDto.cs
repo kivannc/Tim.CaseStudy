42f6c3517f7a59ba1e76afe2f7439cbd7c9a465d
kivanc <erturkkivanc@gmail.com>
2023-01-26 11:32:33 +0300

    BookDto Bookstatus is changed from string to enum

--- LibraryAPI/Dtos/BookDto.cs
+++ LibraryAPI/Dtos/BookDto.cs
@@ -1,14 +1,15 @@
-﻿namespace LibraryAPI.Dtos
+﻿using LibraryAPI.Models;
+
+namespace LibraryAPI.Dtos
 {
     public class BookDto
     {
-
         public string ISBN { get; set; }
 
         public string Name { get; set; }
 
         public string Author { get; set; }
 
-        public string BookStatus { get; set; }
+        public BookStatus BookStatus { get; set; }
     }
 }
