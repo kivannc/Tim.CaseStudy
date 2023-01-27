43d207ce83f0e5353e56e61550d63ea1459903a2
kivanc <erturkkivanc@gmail.com>
2023-01-27 15:46:12 +0300

    BookDetail Ui Improvements

--- library-client/src/components/BookDetail.jsx
+++ library-client/src/components/BookDetail.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
-import { Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Table, Button } from 'react-bootstrap';
+import Currency from '../components/Currency';
+import formatDate from '../utils/FormatDate';
+import { BookStatus, getColor } from '../utils/BookStatus';
 
 const BookDetail = ({ isbn, handleClose }) => {
-  const { data: book } = useQuery({
-    queryKey: ['bookDetails', isbn],
+  const {
+    isLoading,
+    error,
+    data: book,
+  } = useQuery({
+    queryKey: ['book', isbn],
     queryFn: () =>
       axios
         .get(`https://localhost:7133/api/Books/${isbn}`)
@@ -13,18 +20,95 @@ const BookDetail = ({ isbn, handleClose }) => {
     enabled: !!isbn, // only run if bookIsbn is truthy, otherwise don't run
   });
 
+  const { data: transaction, refetch } = useQuery({
+    queryKey: ['bookDetail', book],
+    queryFn: () =>
+      axios
+        .get(`https://localhost:7133/api/Transaction/isbn/${isbn}`)
+        .then((res) => res.data),
+    enabled: false, // only run if bookIsbn is truthy, otherwise don't run
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>An error has occurred: {error.message}</p>;
+  if (book?.bookStatus !== 0) refetch();
+
   return (
-    <>
-      {book ? (
-        <div>
-          <h3>{book.name}</h3>
-          <p>{book.isbn}</p>
-          <p>{book.author}</p>
-        </div>
-      ) : null}
-      <Button onClick={handleClose}>Close</Button>
-    </>
+    <Container>
+      <Row className="mt-2">
+        <Col md={{ span: 8, offset: 2 }}>
+          <Card>
+            <Card.Body>
+              <Card.Title>{book.name}</Card.Title>
+              <Card.Text>{book.isbn}</Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">{book.author}</small>
+              <strong
+                style={{
+                  color: getColor(book.bookStatus),
+                  float: 'right',
+                }}
+              >
+                {BookStatus[book.bookStatus]}
+              </strong>
+            </Card.Footer>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col md={{ span: 8, offset: 2 }}>
+          {transaction && (
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Member</th>
+                  <th>Borrow Date</th>
+                  <th>Due Date</th>
+                  <th>Late Days</th>
+                  <th>Penalty</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    {transaction.member.firstName} {transaction.member.lastName}
+                  </td>
+                  <td>{formatDate(transaction.borrowDate)}</td>
+                  <td>{formatDate(transaction.dueDate)}</td>
+                  <td>{transaction.lateDays}</td>
+                  <td>
+                    <Currency value={transaction.penalty} />
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          )}
+        </Col>
+      </Row>
+      <Row className="mt-2">
+        <Col md={{ span: 8, offset: 2 }}>
+          <Button style={{ float: 'right' }} onClick={handleClose}>
+            Close
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
+
+// return (
+//   <>
+//     {book ? (
+//       <div>
+//         <h3>{book.name}</h3>
+//         <p>{book.isbn}</p>
+//         <p>{book.author}</p>
+//       </div>
+//     ) : null}
+//     <Button onClick={handleClose}>Close</Button>
+//   </>
+// );
+//};
 
 export default BookDetail;
