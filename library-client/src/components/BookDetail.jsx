716b23f0c41dadf97ce83d2b0bb741abfd124dc9
Kivanc ERTURK <erturkkivanc@gmail.com>
2023-01-27 23:07:19 +0300

    Book Detail

--- library-client/src/components/BookDetail.jsx
+++ library-client/src/components/BookDetail.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
-import { Container, Row, Col, Card, Table, Button } from 'react-bootstrap';
-import Currency from '../components/Currency';
-import formatDate from '../utils/FormatDate';
-import { BookStatus, getColor } from '../utils/BookStatus';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import BookTransaction from './BookTransaction';
+import BookCard from './BookCard';
+import BookReserve from './BookReserve';
 
 const BookDetail = ({ isbn, handleClose }) => {
   const {
@@ -15,100 +15,55 @@ const BookDetail = ({ isbn, handleClose }) => {
     queryKey: ['book', isbn],
     queryFn: () =>
       axios
-        .get(`https://localhost:7133/api/Books/${isbn}`)
+        .get(`https://localhost:7133/api/books/${isbn}`)
         .then((res) => res.data),
     enabled: !!isbn, // only run if bookIsbn is truthy, otherwise don't run
   });
 
-  const { data: transaction, refetch } = useQuery({
-    queryKey: ['bookDetail', book],
+  const { data: transaction } = useQuery({
+    queryKey: ['bookDetail', book?.bookStatus],
     queryFn: () =>
       axios
-        .get(`https://localhost:7133/api/Transaction/isbn/${isbn}`)
+        .get(`https://localhost:7133/api/transactions/isbn/${isbn}`)
         .then((res) => res.data),
-    enabled: false, // only run if bookIsbn is truthy, otherwise don't run
+    enabled: !!book?.bookStatus, // only run if bookIsbn is truthy, otherwise don't run
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
   });
+
+  const handleReserve = (userId) => {};
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>An error has occurred: {error.message}</p>;
-  if (book?.bookStatus !== 0) refetch();
+
+  const showReserve = book?.bookStatus === 0;
 
   return (
-    <Container>
+    <>
       <Row className="mt-2">
         <Col md={{ span: 8, offset: 2 }}>
-          <Card>
-            <Card.Body>
-              <Card.Title>{book.name}</Card.Title>
-              <Card.Text>{book.isbn}</Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <small className="text-muted">{book.author}</small>
-              <strong
-                style={{
-                  color: getColor(book.bookStatus),
-                  float: 'right',
-                }}
-              >
-                {BookStatus[book.bookStatus]}
-              </strong>
-            </Card.Footer>
-          </Card>
+          <BookCard book={book} />
         </Col>
       </Row>
       <Row className="mt-2">
         <Col md={{ span: 8, offset: 2 }}>
-          {transaction && (
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Member</th>
-                  <th>Borrow Date</th>
-                  <th>Due Date</th>
-                  <th>Late Days</th>
-                  <th>Penalty</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    {transaction.member.firstName} {transaction.member.lastName}
-                  </td>
-                  <td>{formatDate(transaction.borrowDate)}</td>
-                  <td>{formatDate(transaction.dueDate)}</td>
-                  <td>{transaction.lateDays}</td>
-                  <td>
-                    <Currency value={transaction.penalty} />
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          )}
+          {transaction && <BookTransaction transaction={transaction} />}
         </Col>
       </Row>
       <Row className="mt-2">
         <Col md={{ span: 8, offset: 2 }}>
-          <Button style={{ float: 'right' }} onClick={handleClose}>
-            Close
-          </Button>
+          <Button onClick={handleClose}>Close</Button>
         </Col>
       </Row>
-    </Container>
+
+      <Row className="mt-2">
+        <Col md={{ span: 8, offset: 2 }}>
+          <hr />
+          {!transaction && <BookReserve handleReserve={handleReserve} />}
+        </Col>
+      </Row>
+    </>
   );
 };
-
-// return (
-//   <>
-//     {book ? (
-//       <div>
-//         <h3>{book.name}</h3>
-//         <p>{book.isbn}</p>
-//         <p>{book.author}</p>
-//       </div>
-//     ) : null}
-//     <Button onClick={handleClose}>Close</Button>
-//   </>
-// );
-//};
 
 export default BookDetail;
